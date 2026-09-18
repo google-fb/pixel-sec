9e76f0b56c567b693e4332ec6845cd32f8939cc7
google-fb <154669670+google-fb@users.noreply.github.com>
2026-09-18 23:54:59 +0800

    Fix main.tsx JSX tags after unicode-escape artifact

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 injectZpixFont()
 
 createRoot(document.getElementById('root')!).render(
-  \u003cStrictMode\u003e
-    \u003cApp /\u003e
-  \u003c/StrictMode\u003e,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
