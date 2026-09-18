dd35472b1a1a8fcde871aa2ac490f1947b98503e
google-fb <154669670+google-fb@users.noreply.github.com>
2026-09-18 23:54:33 +0800

    Inject Zpix pixel font before React mount

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { injectZpixFont } from './assets/zpix/inject'
 import './index.css'
 import App from './App.tsx'
 
+injectZpixFont()
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  \u003cStrictMode\u003e
+    \u003cApp /\u003e
+  \u003c/StrictMode\u003e,
 )
